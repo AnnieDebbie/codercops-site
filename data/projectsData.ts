48063babe7665c8d20057952cdfdeda287d51bee
anurag629 <akvermaav629@gmail.com>
2023-09-05 10:04:54 +0530

    updated my peronal details

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,11 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Fruits and Vegetables Classifier',
+    description: `Developed robust Deep Learning model using Transfer Learning and fine-tuning techniques to accurately classify fruits and vegetables from images, achieving a remarkable accuracy of over 98%. Utilized TensorFlow and Keras frameworks to implement the model architecture and performed extensive data preprocessing and augmentation techniques.    `,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: '',
   },
   {
     title: 'The Time Machine',
@@ -14,7 +12,7 @@ const projectsData = [
     forgotten headphones with this simple yet affordable solution.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
-  },
+  }
 ]
 
 export default projectsData
