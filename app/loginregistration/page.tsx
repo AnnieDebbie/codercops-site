1250261b286cb521c0403f6b7f5e0e30adee5d83
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-10-07 01:38:08 +0100

    Create loginregistration page

---
+++ app/loginregistration/page.tsx
@@ -0,0 +1,13 @@
+import LoginRegistration from '@/components/authentication/LoginRegistration'
+
+export default function LoginRegistrationPage() {
+  return (
+    <>
+      <div className="divid e-y divide-gray-200 dark:divide-gray-700">
+        <div className="py-5 mx-auto">
+          <LoginRegistration />
+        </div>
+      </div>
+    </>
+  )
+}
