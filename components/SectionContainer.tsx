00b9c17b9cf4c61a08c3275a794cc52bbdd5f4b1
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-10-07 01:29:51 +0100

    Dynamically set width of section container

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -1,11 +1,24 @@
+'use client'
+
 import { ReactNode } from 'react'
+import { usePathname } from 'next/navigation'
 
 interface Props {
   children: ReactNode
 }
 
 export default function SectionContainer({ children }: Props) {
+  const pathname = usePathname()
+
   return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>
+    <section
+      className={` ${
+        pathname === '/loginregistration'
+          ? 'container mx-auto px-4 '
+          : 'mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'
+      }`}
+    >
+      {children}
+    </section>
   )
 }
